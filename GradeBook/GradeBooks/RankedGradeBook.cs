e010965b417669c13f51492c99d568a8f9c5e6d8
Kenneth Mwangi <kemwangi@microsoft.com>
2021-05-13 23:00:04 +0300

    Now using Linq and example

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,60 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        double[] SortedGrades;
-        double TwentyPercent;
-
+       
 
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
-            SortedGrades = new double[Students.Count];
-            TwentyPercent = Students.Count * 0.2;
-            PreCalculateGrades();
+          
         }
 
-        void PreCalculateGrades()
-        {
-         
-            int index = 0;
-            foreach (Student student in Students)
-            {
-                SortedGrades[index] = student.AverageGrade;
-                index++;
-            }
-
-            Array.Sort(SortedGrades);
-        }
-
-        char DetermineGradeGroup(double averageGrade)
-        {
-            char[] grades = new [] { 'A', 'B', 'C', 'D', 'F'};
-            int start = 0;
-            int count = 0;
-            foreach(double average in SortedGrades)
-            {
-                if(count + 1 > TwentyPercent)
-                {
-                    TwentyPercent *= 2;
-                    start++;
-                }
-                count++;
-
-            }
-
-            return grades[start];
-            
-        }
+       
 
         public override char GetLetterGrade(double averageGrade)
         {
             CheckCanCalculate();
-            return DetermineGradeGroup(averageGrade);
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (averageGrade >= grades[threshold - 1])
+                return 'A';
+            else if (averageGrade >= grades[threshold - 2])
+                return 'B';
+
+            return 'F';
         }
 
         void CheckCanCalculate()
