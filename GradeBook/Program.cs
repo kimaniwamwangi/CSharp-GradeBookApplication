4981ac4f8d207f4e377ea9402da449c004ba089f
Kenneth Mwangi <kemwangi@microsoft.com>
2021-05-13 21:49:38 +0300

    Testing commit works

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -8,7 +8,7 @@ namespace GradeBook
         static void Main(string[] args)
         {
             Console.WriteLine("#=======================#");
-            Console.WriteLine("# Welcome to GradeBook! #");
+            Console.WriteLine("# Welcome to GradeBook! By Kenneth #");
             Console.WriteLine("#=======================#");
 
             StartingUserInterface.CommandLoop();
